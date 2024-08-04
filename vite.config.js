832557458fb697a7506ea35237063c799d641327
zoe-shih <zoe76102469@gmail.com>
2024-08-04 16:49:04 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/',
+  base: '/2024_Week03_04/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
